aec3dab9b113a666055b88719f47551dbe9a42a7
Abhay <abhay.khatariya@brainvire.com>
2025-05-29 14:02:59 +0530

    Delete NewsletterPopup directory

--- NewsletterPopup/registration.php
+++
@@ -1,3 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AllPurposeCovers_NewsletterPopup', __DIR__); 
--- NewsletterPopup/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'apcNewsletterPopup': 'AllPurposeCovers_NewsletterPopup/js/popup'
-        }
-    }
-}; 
